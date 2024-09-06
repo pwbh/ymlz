0d67204b344087e8c3d8d91d020b92dda08894bc
pwbh <denispolupan@icloud.com>
2024-09-06 17:46:50 +0300

    pass test casea

--- src/tests.zig
+++ src/tests.zig
@@ -31,3 +31,31 @@ test "98YD" {
 
     try expect(std.mem.eql(u8, result.elements[0].name, "Spec Example 5.5. Comment Indicator"));
 }
+
+test "CC74" {
+    const Element = struct {
+        name: []const u8,
+        from: []const u8,
+        tags: []const u8,
+        yaml: []const u8,
+        tree: []const u8,
+        json: []const u8,
+        dump: []const u8,
+    };
+
+    const Experiment = struct {
+        elements: []Element,
+    };
+
+    const yml_file_location = try std.fs.cwd().realpathAlloc(
+        std.testing.allocator,
+        "./resources/yaml-test-suite/CC74.yml",
+    );
+    defer std.testing.allocator.free(yml_file_location);
+
+    var ymlz = try Ymlz(Experiment).init(std.testing.allocator);
+    const result = try ymlz.loadFile(yml_file_location);
+    defer ymlz.deinit(result);
+
+    try expect(std.mem.eql(u8, result.elements[0].name, "Spec Example 6.20. Tag Handles"));
+}
