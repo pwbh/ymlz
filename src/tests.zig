55f90f50e7b1600f654c6d5554a0124805888a83
pwbh <denispolupan@icloud.com>
2024-09-14 12:22:58 +0300

    QT73 test

--- src/tests.zig
+++ src/tests.zig
@@ -98,3 +98,34 @@ test "F6MC" {
     try expect(std.mem.eql(u8, element.name, "More indented lines at the beginning of folded block scalars"));
     try expect(std.mem.eql(u8, element.json, "{\n  \"a\": \" more indented\\nregular\\n\",\n  \"b\": \"\\n\\n more indented\\nregular\\n\"\n}"));
 }
+
+test "QT73" {
+    const Element = struct {
+        name: []const u8,
+        from: []const u8,
+        tags: []const u8,
+        yaml: []const u8,
+        tree: []const u8,
+        json: []const u8,
+        dump: []const u8,
+    };
+
+    const Experiment = struct {
+        elements: []Element,
+    };
+
+    const yml_file_location = try std.fs.cwd().realpathAlloc(
+        std.testing.allocator,
+        "./resources/yaml-test-suite/QT73.yml",
+    );
+    defer std.testing.allocator.free(yml_file_location);
+
+    var ymlz = try Ymlz(Experiment).init(std.testing.allocator);
+    const result = try ymlz.loadFile(yml_file_location);
+    defer ymlz.deinit(result);
+
+    const element = result.elements[0];
+
+    try expect(std.mem.eql(u8, element.name, "Comment and document-end marker"));
+    try expect(std.mem.eql(u8, element.from, "@perlpunk"));
+}
